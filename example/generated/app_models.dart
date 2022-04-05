370546455ab2a54458c6c6fd310a80ef3e3f8e49
Parables <parables95@gmail.com>
2022-04-05 08:47:20 +0000

    last checkpoint

--- example/generated/app_models.dart
+++ example/generated/app_models.dart
@@ -6,34 +6,34 @@ part 'app_models.g.dart';
 
 
 @freezed
- class AInput with _$AInput{
+class AInput with _$AInput{
   const factory AInput({
     required BInput b,
   }) = _AInput;
 
 factory AInput.fromJson(Map<String, dynamic> json) => _$AInputFromJson(json);
-};
+}
 
 @freezed
- class BInput with _$BInput{
+class BInput with _$BInput{
   const factory BInput({
     required CInput c,
   }) = _BInput;
 
 factory BInput.fromJson(Map<String, dynamic> json) => _$BInputFromJson(json);
-};
+}
 
 @freezed
- class CInput with _$CInput{
+class CInput with _$CInput{
   const factory CInput({
     required AInput a,
   }) = _CInput;
 
 factory CInput.fromJson(Map<String, dynamic> json) => _$CInputFromJson(json);
-};
+}
 
 @freezed
- class Character with _$Character{
+class Character with _$Character{
   const factory Character({
     required List<Episode?> appearsIn,
      List<Character?>? friends,
@@ -42,10 +42,10 @@ factory CInput.fromJson(Map<String, dynamic> json) => _$CInputFromJson(json);
   }) = _Character;  
 
 factory Character.fromJson(Map<String, dynamic> json) => _$CharacterFromJson(json);
-};
+}
 
 @freezed
- class ComplexType with _$ComplexType{
+class ComplexType with _$ComplexType{
   const factory ComplexType({
      List<String?>? a,
      List<String>? b,
@@ -59,28 +59,28 @@ factory Character.fromJson(Map<String, dynamic> json) => _$CharacterFromJson(jso
   }) = _ComplexType;  
 
 factory ComplexType.fromJson(Map<String, dynamic> json) => _$ComplexTypeFromJson(json);
-};
+}
 
 @freezed
- class CreateMovieInput with _$CreateMovieInput{
+class CreateMovieInput with _$CreateMovieInput{
   const factory CreateMovieInput({
     required String title,
   }) = _CreateMovieInput;
 
 factory CreateMovieInput.fromJson(Map<String, dynamic> json) => _$CreateMovieInputFromJson(json);
-};
+}
 
 @freezed
- class DeleteMovieInput with _$DeleteMovieInput{
+class DeleteMovieInput with _$DeleteMovieInput{
   const factory DeleteMovieInput({
     required String id,
   }) = _DeleteMovieInput;
 
 factory DeleteMovieInput.fromJson(Map<String, dynamic> json) => _$DeleteMovieInputFromJson(json);
-};
+}
 
 @freezed
- class Droid with _$Droid{
+class Droid with _$Droid{
   const factory Droid({
     required List<Episode?> appearsIn,
      List<Character?>? friends,
@@ -90,7 +90,7 @@ factory DeleteMovieInput.fromJson(Map<String, dynamic> json) => _$DeleteMovieInp
   }) = _Droid;  
 
 factory Droid.fromJson(Map<String, dynamic> json) => _$DroidFromJson(json);
-};
+}
 
 enum Episode {
    EMPIRE,
@@ -99,7 +99,7 @@ enum Episode {
 }
 
 @freezed
- class Human with _$Human{
+class Human with _$Human{
   const factory Human({
     required List<Episode?> appearsIn,
      List<Character?>? friends,
@@ -110,10 +110,10 @@ enum Episode {
   }) = _Human;  
 
 factory Human.fromJson(Map<String, dynamic> json) => _$HumanFromJson(json);
-};
+}
 
 @freezed
- class Movie with _$Movie{
+class Movie with _$Movie{
   const factory Movie({
     required String id,
     required String title,
@@ -133,10 +133,10 @@ const factory Movie.deleteInput({
 }) = _DeleteMovieInput;
 
 factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
-};
+}
 
 @freezed
- class SearchResult with _$SearchResult{
+class SearchResult with _$SearchResult{
 
 const factory SearchResult.droid({ 
     required List<Episode?> appearsIn,
@@ -163,10 +163,10 @@ const factory SearchResult.starship({
 
 
 factory SearchResult.fromJson(Map<String, dynamic> json) => _$SearchResultFromJson(json);
-};
+}
 
 @freezed
- class Starship with _$Starship{
+class Starship with _$Starship{
   const factory Starship({
     required String id,
      double? length,
@@ -174,24 +174,24 @@ factory SearchResult.fromJson(Map<String, dynamic> json) => _$SearchResultFromJs
   }) = _Starship;  
 
 factory Starship.fromJson(Map<String, dynamic> json) => _$StarshipFromJson(json);
-};
+}
 
 @freezed
- class UpdateMovieInput with _$UpdateMovieInput{
+class UpdateMovieInput with _$UpdateMovieInput{
   const factory UpdateMovieInput({
     required String id,
      String? title,
   }) = _UpdateMovieInput;
 
 factory UpdateMovieInput.fromJson(Map<String, dynamic> json) => _$UpdateMovieInputFromJson(json);
-};
+}
 
 @freezed
- class UpsertMovieInput with _$UpsertMovieInput{
+class UpsertMovieInput with _$UpsertMovieInput{
   const factory UpsertMovieInput({
     required String id,
     required String title,
   }) = _UpsertMovieInput;
 
 factory UpsertMovieInput.fromJson(Map<String, dynamic> json) => _$UpsertMovieInputFromJson(json);
-};
+}
