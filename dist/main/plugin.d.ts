0c25e63b66710e45744e8c4386a961a3787e5010
Parables <parables95@gmail.com>
2022-04-11 18:51:23 +0000

    last checkpoint

--- dist/main/plugin.d.ts
+++
@@ -1,3 +0,0 @@
-import { PluginFunction, Types } from '@graphql-codegen/plugin-helpers';
-import { FlutterFreezedClassPluginConfig } from './config';
-export declare const plugin: PluginFunction<FlutterFreezedClassPluginConfig, Types.PluginOutput>;
